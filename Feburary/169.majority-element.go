a2d7440cdaba8714bd9b2667bdbcc2d1ef76972b
BrightOlawale <oolawalebright@gmail.com>
2024-02-12 15:06:39 +0100

    Leetcode: Majority element

---
+++ Feburary/169.majority-element.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=169 lang=golang
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+func majorityElement(nums []int) int {
+	count := 0
+	var candidate int
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			count ++
+		} else {
+			count --
+		}
+	}
+
+	return candidate
+}
+// @lc code=end
+
