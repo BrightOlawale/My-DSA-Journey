3aeb853d6e47049aa6ee41135d9f3d2c368db1bb
BrightOlawale <oolawalebright@gmail.com>
2024-02-20 23:57:21 +0100

    Leetcode: missingNumber

--- Feburary/268.missing-number.go
+++ Feburary/268.missing-number.go
@@ -1,5 +1,7 @@
 package feburary
 
+import "sort"
+
 /*
  * @lc app=leetcode id=268 lang=golang
  *
@@ -7,6 +9,18 @@ package feburary
  */
 
 // @lc code=start
+func missingNumber(nums []int) int {
+    sort.Ints(nums)
+    for n:=len(nums); n >= 0; n-- {
+        if n-1 >= 0 && n == nums[n-1]{
+            continue
+        } else{
+            return n
+        }
+    }
+    return 0
+}
+
 func missingNumber(nums []int) int {
     missing := len(nums) 
     
