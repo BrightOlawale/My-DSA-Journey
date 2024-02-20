98402d4964fbc8a4fe786f6d7b923499e806572a
BrightOlawale <oolawalebright@gmail.com>
2024-02-21 00:01:14 +0100

    Leetcode: missingNumber

--- Feburary/268.missing-number.go
+++ Feburary/268.missing-number.go
@@ -10,6 +10,26 @@ import "sort"
 
 // @lc code=start
 func missingNumber(nums []int) int {
+    // Compute the length of nums
+    n := len(nums)
+
+    // Initialize the difference of sum by n
+    differenceOfSum := n
+
+    // Run a loop from 0 to (n - 1)
+    for i := 0; i < n; i++ {
+        // Add to the difference of sum the number i
+        // and subtract the number at index i in nums
+        differenceOfSum += (i - nums[i])
+    }
+
+    // After adding all of the differences only
+    // the missing number will remain in the difference of sum
+    return differenceOfSum
+}
+
+
+func missingNumber(nums []int) int {
     sort.Ints(nums)
     for n:=len(nums); n >= 0; n-- {
         if n-1 >= 0 && n == nums[n-1]{
