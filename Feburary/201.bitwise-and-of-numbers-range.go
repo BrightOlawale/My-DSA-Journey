57c67de3dd62c31bd57a30680fbcdffdb66f298b
BrightOlawale <oolawalebright@gmail.com>
2024-02-21 23:11:07 +0100

    Leetcode: rangeBitwiseAnd

---
+++ Feburary/201.bitwise-and-of-numbers-range.go
@@ -0,0 +1,31 @@
+package feburary
+
+/*
+ * @lc app=leetcode id=201 lang=golang
+ *
+ * [201] Bitwise AND of Numbers Range
+ */
+
+// @lc code=start
+func rangeBitwiseAnd(left int, right int) int {
+	func rangeBitwiseAnd(left, right int) int {
+		shift := 0
+		for left < right {
+			left >>= 1 // Right shift left
+			right >>= 1 // Right shift right
+			shift++
+		}
+		return left << shift
+	}
+}
+
+func rangeBitwiseAnd(left, right int) int {
+    for left < right {
+        right &= (right - 1) // Clear the rightmost set bit of right
+    }
+    return right
+}
+
+
+// @lc code=end
+
