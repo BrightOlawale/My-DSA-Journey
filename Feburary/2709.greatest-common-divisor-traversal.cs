4b1968e44e5322485a6d7850dd3176cfcb852558
BrightOlawale <oolawalebright@gmail.com>
2024-02-25 23:46:03 +0100

    Leetcode: canTraverseAllPairs

---
+++ Feburary/2709.greatest-common-divisor-traversal.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2709 lang=csharp
+ *
+ * [2709] Greatest Common Divisor Traversal
+ */
+
+// @lc code=start
+public class Solution {
+    public bool CanTraverseAllPairs(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
