15472370eb2f20121f76079c50b16171be582f05
BrightOlawale <oolawalebright@gmail.com>
2024-03-07 23:35:26 +0100

    Leetcode: middleNode

---
+++ March/876-middle-of-the-link-list.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func middleNode(head *ListNode) *ListNode {
+    if head == nil {
+        return head
+    }
+
+    fast := head
+    slow := head
+
+    for fast.Next != nil {
+        fast = fast.Next
+
+        if fast.Next != nil {
+            fast = fast.Next
+        }
+        slow = slow.Next
+    }
+
+    return slow
+}
